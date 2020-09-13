1eab5cf7796cabb136ace0400e437d9bd208d26d
Marco <marcoaluchi@hotmail.com>
2020-09-12 21:27:28 -0300

    se agregaron nuevas route

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ import Pelicula from "./components/Pelicula";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Idpelicula from "./components/Idpelicula";
+import Navegacion from "./components/Navegacion";
+import Contacto from "./components/Contacto";
+import Links from "./components/Links";
+import Tutorial from "./components/Tutorial";
 import Footer from "./components/Footer";
 function App() {
   return (
     <Router>
       <PeliculasProvider>
-        <Container className="minheight">
-          <Jumbotron>
+        <Navegacion />
+        <Container className="altura mt-5">
+          <Jumbotron className="col-12">
             <Switch>
               <Route exact path="/" component={Pelicula} />
-              <Route path="/:id" component={Idpelicula} />
+              <Route exact path="/contacto" component={Contacto} />
+              <Route exact path="/links" component={Links} />
+              <Route exact path="/tutorial" component={Tutorial} />
+              <Route exact path="/:id" component={Idpelicula} />
             </Switch>
           </Jumbotron>
           <Footer />
