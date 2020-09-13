1c24caf556ac07ea6dd2f946b76f2e88ac18bcee
Marco <marcoaluchi@hotmail.com>
2020-09-13 18:50:24 -0300

    se agrego formulario, falta enviarlo a google stylesheet

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 const Contacto = () => {
-  return <h2>Holis desde Contacto</h2>;
+  const handleClick = () => {};
+  return (
+    <Form>
+      <h1>Contacto</h1>
+      <p>
+        Si se te ocurre una idea para mejorar algo, o algun error o
+        inconveniente, no dudes en hacermelo saber
+      </p>
+      <Form.Group controlId="exampleForm.ControlInput1">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="nombre@ejemplo.com" />
+      </Form.Group>
+      <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Consulta</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows="3"
+          placeholder="Escribe tu consulta aqui..."
+        />
+      </Form.Group>
+      <Button variant="info" onClick={handleClick}>
+        Enviar
+      </Button>
+    </Form>
+  );
 };
 
 export default Contacto;
