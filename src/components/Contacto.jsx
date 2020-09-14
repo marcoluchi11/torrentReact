f47405fc5708b98bfe6ec5c89debb4f0edad118a
Marco <marcoaluchi@hotmail.com>
2020-09-13 21:26:45 -0300

    ya funciona el formulario correctamente

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { enviarFormulario } from "./envioContacto";
+import { PeliculasContext } from "../context/PeliculasContext";
 const Contacto = () => {
-  const handleClick = () => {};
+  const { consulta, setConsulta } = useContext(PeliculasContext);
+
+  const { Mail, Consulta } = consulta;
+  const handleChange = (e) => {
+    setConsulta({ ...consulta, [e.target.name]: e.target.value });
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    enviarFormulario({
+      Mail: Mail,
+      Consulta: Consulta,
+    });
+    setConsulta({
+      Mail: "",
+      Consulta: "",
+    });
+  };
   return (
     <Form>
       <h1>Contacto</h1>
@@ -12,11 +30,20 @@ const Contacto = () => {
       </p>
       <Form.Group controlId="exampleForm.ControlInput1">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="nombre@ejemplo.com" />
+        <Form.Control
+          value={Mail}
+          onChange={handleChange}
+          name="Mail"
+          type="email"
+          placeholder="nombre@ejemplo.com"
+        />
       </Form.Group>
       <Form.Group controlId="exampleForm.ControlTextarea1">
         <Form.Label>Consulta</Form.Label>
         <Form.Control
+          value={Consulta}
+          onChange={handleChange}
+          name="Consulta"
           as="textarea"
           rows="3"
           placeholder="Escribe tu consulta aqui..."
