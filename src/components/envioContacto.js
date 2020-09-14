4d0237e65e3f9684591438e422cfbcd1b50e65de
Marco <marcoaluchi@hotmail.com>
2020-09-13 21:27:05 -0300

    se creo correctamente la funcion que envia al stylesheet las dudas

--- src/components/envioContacto.js
+++ src/components/envioContacto.js
@@ -1,5 +1,18 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
 
-const doc = new GoogleSpreadsheet(
-  "1Wg4PLg1nSOjbnjJAXQUoE_A6-fdOiMlcvCsK-xyL348"
-);
+const doc = new GoogleSpreadsheet(process.env.REACT_APP_GOOGLE_ID);
+
+export async function enviarFormulario(row) {
+  try {
+    await doc.useServiceAccountAuth({
+      client_email: process.env.REACT_APP_EMAIL,
+      private_key: process.env.REACT_APP_PRIVATE_KEY,
+    });
+    await doc.loadInfo();
+
+    const sheet = doc.sheetsById[955052155];
+    const result = await sheet.addRow(row);
+  } catch (e) {
+    console.error("Error: ", e);
+  }
+}
