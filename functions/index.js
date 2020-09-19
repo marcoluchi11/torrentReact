dd3018099c1a2983cf6b0373ca05dcce1bfd39ea
Marco <marcoaluchi@hotmail.com>
2020-09-19 17:24:27 -0300

    se agrego functions de Firebase para alojar api de torrents

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const functions = require("firebase-functions");
+
+const express = require("express");
+const TorrentSearchApi = require("torrent-search-api");
+const app = express();
+
+TorrentSearchApi.enableProvider("Yts");
+TorrentSearchApi.enableProvider("ThePirateBay");
+
+const traerLinks = async (nombre) => {
+  const torrents = await TorrentSearchApi.search(nombre, "All", 10);
+
+  return torrents;
+};
+
+app.get("/torrent/:nombre", (req, res) => {
+  traerLinks(req.params.nombre)
+    .then((result) => res.json(result))
+    .catch((err) => console.log("Hubo un error al realizar el pedido", err));
+});
+
+exports.app = functions.https.onRequest(app);
