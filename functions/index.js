2ff6320cea590e796fbc6d8d5a3da4deca48990e
Marco <marcoaluchi@hotmail.com>
2020-09-19 21:39:28 -0300

    se agrego cors a las dependencies para no tener problemas en los fetcheos

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const functions = require("firebase-functions");
-
+let cors = require("cors");
 const express = require("express");
 const TorrentSearchApi = require("torrent-search-api");
 const app = express();
-
+app.use(cors());
 TorrentSearchApi.enableProvider("Yts");
 TorrentSearchApi.enableProvider("ThePirateBay");
 
