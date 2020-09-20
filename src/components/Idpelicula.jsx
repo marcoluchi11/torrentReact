0b643cb81e9a20790721abb4ea2a8f29567a892b
Marco <marcoaluchi@hotmail.com>
2020-09-19 21:39:15 -0300

    se agrego uuid para la key del .map()

--- src/components/Idpelicula.jsx
+++ src/components/Idpelicula.jsx
@@ -4,25 +4,25 @@ import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Torrent from "./Torrent";
 import Loading from "./Loading";
 import flechita from "./../images/flechaatras.svg";
 import styled from "@emotion/styled";
 import TablaTorrent from "./TablaTorrent";
+import { v4 as uuidv4 } from "uuid";
 const ImagenPelicula = styled.div`
   display: flex;
   justify-content: center;
   align-items: flex-start;
 `;
 const Idpelicula = () => {
-  const { setSeleccion, info, setInfo, setTorrent } = useContext(
+  const { setSeleccion, info, setInfo, setBusqTorrent } = useContext(
     PeliculasContext
   );
   //const { Title, Poster, Year, Director, Plot, Runtime, Metascore } = info;
   const handleClick = () => {
     setSeleccion({});
     setInfo(null);
-    setTorrent(null);
+    setBusqTorrent([]);
   };
   return (
     <Fragment>
@@ -40,10 +40,24 @@ const Idpelicula = () => {
               <h1>{info.Title}</h1>
               <h3>Año: {info.Year}</h3>
               <h3> Duracion : {info.Runtime}</h3>
+              <h3>Genero: {info.Genre}</h3>
+              <h4>Actores principales : {info.Actors}</h4>
+              <h4>Premios : {info.Awards}</h4>
+              {/* #EFE3A4 */}
               <h5>Director: {info.Director}</h5>
+              <h5>Escritor: {info.Writer}</h5>
               <p>{info.Plot}</p>
-              <p>Puntuacion : {info.Metascore}</p>
-              <Torrent />
+              <p>
+                Ratings:
+                {info.Ratings.map((rating) => (
+                  <p key={uuidv4()}>
+                    {rating.Source}: {rating.Value}
+                  </p>
+                ))}
+              </p>
+              <p>iMdB Rating: {info.imdbRating}</p>
+              <p>BoxOffice: {info.BoxOffice}</p>
+              <p>Produccion: {info.Production}</p>
               <Link to="/">
                 <Button variant="secondary" onClick={handleClick} block>
                   <img className="mr-2" src={flechita} alt="" />
