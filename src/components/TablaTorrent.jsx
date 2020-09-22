836634611432689fafa2e5d9dd1aaabddb21b149
Marco <marcoaluchi@hotmail.com>
2020-09-22 15:21:03 -0300

    se agrego paginacion y se cambio disenio de la info para mobile

--- src/components/TablaTorrent.jsx
+++ src/components/TablaTorrent.jsx
@@ -1,34 +1,51 @@
 import React, { Fragment, useContext } from "react";
 import { PeliculasContext } from "../context/PeliculasContext";
+
+import PaginacionTabla from "./PaginacionTabla";
 import Table from "react-bootstrap/Table";
+import Error from "./Error";
+import { v4 as uuidv4 } from "uuid";
 const TablaTorrent = () => {
-  const { busqTorrent } = useContext(PeliculasContext);
+  const { busqTorrent, setError, error } = useContext(PeliculasContext);
+
+  const valores = busqTorrent.map((opcion) => (
+    <tr key={uuidv4()}>
+      <td>
+        <a href={opcion.magnet || opcion.link || opcion.desc}>{opcion.title}</a>
+      </td>
+      <td>{opcion.seeds}</td>
+      <td>{opcion.peers || "-"}</td>
+      <td>{opcion.size}</td>
+    </tr>
+  ));
+  if (valores.length === 0) {
+    setError(true);
+  } else {
+    setError(false);
+  }
   return (
     <Fragment>
-      <Table className="mt-3" striped bordered hover variant="dark">
-        <thead>
-          <tr>
-            <th>Titulo</th>
-            <th>Seeds</th>
-            <th>Leechers</th>
-            <th>Tamaño</th>
-          </tr>
-        </thead>
-        <tbody>
-          {busqTorrent.map((opcion) => (
-            <tr key={opcion.id}>
-              <td>
-                <a href={opcion.magnet || opcion.link || opcion.desc}>
-                  {opcion.title}
-                </a>
-              </td>
-              <td>{opcion.seeds}</td>
-              <td>{opcion.peers || "-"}</td>
-              <td>{opcion.size}</td>
+      {error ? (
+        <Error mensaje="No se encontraron Torrents" />
+      ) : (
+        <table className="mt-3">
+          <thead>
+            <tr>
+              <th>Titulo</th>
+              <th>Seeds</th>
+              <th>Leechers</th>
+              <th>Tamaño</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+
+          <PaginacionTabla
+            itemsperpage={6}
+            nocolumns={5}
+            items={valores}
+            pagesspan={10}
+          />
+        </table>
+      )}
     </Fragment>
   );
 };
