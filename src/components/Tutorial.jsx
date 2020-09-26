611bec70003582f1b4c93ecf5abf1ee472fe9349
Marco <marcoaluchi@hotmail.com>
2020-09-26 10:24:52 -0300

    cambio de disenio y destacado

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -1,35 +1,72 @@
-import React, { Fragment } from "react";
+import React from "react";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  background-color: #686868;
+  -webkit-box-shadow: 10px 10px 16px -8px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 10px 10px 16px -8px rgba(0, 0, 0, 0.75);
+  box-shadow: 10px 10px 16px -8px rgba(0, 0, 0, 0.75);
+  border-radius: 10px;
+  padding: 1.6rem;
+`;
+const PasosTitulo = styled.h1`
+  font-weight: 300;
+  text-decoration: underline;
+  padding-bottom: 0.7 rem;
+`;
+const Parrafo = styled.p`
+  font-weight: 300;
+`;
+
+const Linkovich = styled.a`
+  text-transform: underline !important;
+`;
 const Tutorial = () => {
   return (
-    <Fragment>
+    <Contenedor>
       <h1>Como Descargar</h1>
-      <h3>Paso 1</h3>
+      <PasosTitulo>Paso 1</PasosTitulo>
       <p>Descargar el cliente de Torrent</p>
       <p>qBittorrent</p>
-      <h3>Paso 2</h3>
-      <p>
-        {" "}
-        Buscar peliculas en Pagina principal o en las paginas de torrents que
-        estan en Links de Utilidad
-      </p>
-      <h3>Paso 3</h3>
-      <p>
+      <img
+        className="mb-2"
+        src="https://img.utdstc.com/icons/qbittorrent-windows.png:150"
+        alt=""
+      />
+      <div className="col-12 ">
+        <Linkovich href="https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent_4.2.5_setup.exe">
+          32-bit
+        </Linkovich>{" "}
+        <Linkovich href="https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent_4.2.5_x64_setup.exe">
+          64-bit
+        </Linkovich>{" "}
+        <Linkovich href="https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent-4.2.5.dmg">
+          Mac OS
+        </Linkovich>
+      </div>
+
+      <PasosTitulo>Paso 2</PasosTitulo>
+      <Parrafo> Buscar peliculas en la Home Page</Parrafo>
+      <PasosTitulo>Paso 3</PasosTitulo>
+      <Parrafo>
         Buscar pelicula y seleccionar la calidad (720p o 1080p en lo posible){" "}
-      </p>
-      <h3>Paso 4</h3>
-      <p>
+      </Parrafo>
+      <PasosTitulo>Paso 4</PasosTitulo>
+      <Parrafo>
         Se va a descargar un archivo, ese archivo lo abris con el qBittorrent,
         le das ok. Y empezara la descarga.
-      </p>
-      <h3>Paso 5</h3>
-      <p>
-        Luego de descargada la pelicula, faltan los subtitulos. Aqui se pueden
-        hacer dos cosas, o bien ir a alguna pagina de subtitulos que deje en
-        "Links de Utilidad" o bien, En el Reproductor VLC hay un apartado que se
-        llama VLSub que te busca los subtitulos ahi mismo en el reproductor por
-        idioma. Buscar que la sincronizacion de los textos sea buena.
-      </p>
-    </Fragment>
+      </Parrafo>
+      <PasosTitulo>Paso 5</PasosTitulo>
+      <Parrafo>
+        Luego de descargada la pelicula, faltaria descargar los subtitulos. Aqui
+        se pueden hacer dos cosas: <br /> Ir a alguna pagina de subtitulos como
+        por ejemplo: Subdivx, Argen-team, Tusubtitulo o bien,
+        <br /> en el Reproductor VLC hay un apartado que se llama VLSub que
+        busca los subtitulos ahi mismo en el reproductor por idioma.
+        <br />
+        Buscar que la sincronizacion de los textos sea buena.
+      </Parrafo>
+    </Contenedor>
   );
 };
 
