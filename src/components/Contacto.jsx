1f3bfe7e9e9ab0a4c8067e672cc4ecd573ca987f
Marco <marcoaluchi@hotmail.com>
2020-09-27 18:52:59 -0300

    se le agrego opacity al contenedor

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -2,9 +2,19 @@ import React, { useContext } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Error from "./Error";
+import styled from "@emotion/styled";
 import { enviarFormulario } from "./envioContacto";
 import { PeliculasContext } from "../context/PeliculasContext";
 import MensajeExito from "./MensajeExito";
+const ContenedorSecciones = styled.div`
+  background-color: #686868;
+  -webkit-box-shadow: 10px 10px 16px -8px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 10px 10px 16px -8px rgba(0, 0, 0, 0.75);
+  box-shadow: 10px 10px 16px -8px rgba(0, 0, 0, 0.75);
+  border-radius: 10px;
+  padding: 1.6rem;
+  opacity: 0.9;
+`;
 const Contacto = () => {
   const {
     consulta,
@@ -41,41 +51,43 @@ const Contacto = () => {
     }, 4000);
   };
   return (
-    <Form>
-      <h1>Contacto</h1>
-      <p>
-        Si se te ocurre una idea para mejorar algo, o algun error o
-        inconveniente, no dudes en hacermelo saber
-      </p>
-      <Form.Group controlId="exampleForm.ControlInput1">
-        <Form.Label className="letraContacto">Email</Form.Label>
-        <Form.Control
-          value={Mail}
-          onChange={handleChange}
-          name="Mail"
-          type="email"
-          placeholder="nombre@ejemplo.com"
-        />
-      </Form.Group>
-      {error ? <Error mensaje="Ingrese un valor correcto" /> : null}
-      <Form.Group controlId="exampleForm.ControlTextarea1">
-        <Form.Label className="letraContacto">Consulta</Form.Label>
-        <Form.Control
-          value={Consulta}
-          onChange={handleChange}
-          name="Consulta"
-          as="textarea"
-          rows="3"
-          placeholder="Escribe tu consulta aqui..."
-        />
-      </Form.Group>
-      <Button className="mb-2" variant="info" onClick={handleClick}>
-        Enviar
-      </Button>
-      {enviado ? (
-        <MensajeExito mensaje="El mensaje se envio con exito" />
-      ) : null}
-    </Form>
+    <ContenedorSecciones>
+      <Form>
+        <h1>Contacto</h1>
+        <p>
+          Si se te ocurre una idea para mejorar algo, o algun error o
+          inconveniente, no dudes en hacermelo saber
+        </p>
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label className="letraContacto">Email</Form.Label>
+          <Form.Control
+            value={Mail}
+            onChange={handleChange}
+            name="Mail"
+            type="email"
+            placeholder="nombre@ejemplo.com"
+          />
+        </Form.Group>
+        {error ? <Error mensaje="Ingrese un valor correcto" /> : null}
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label className="letraContacto">Consulta</Form.Label>
+          <Form.Control
+            value={Consulta}
+            onChange={handleChange}
+            name="Consulta"
+            as="textarea"
+            rows="3"
+            placeholder="Escribe tu consulta aqui..."
+          />
+        </Form.Group>
+        <Button className="mb-2" variant="info" onClick={handleClick}>
+          Enviar
+        </Button>
+        {enviado ? (
+          <MensajeExito mensaje="El mensaje se envio con exito" />
+        ) : null}
+      </Form>
+    </ContenedorSecciones>
   );
 };
 
